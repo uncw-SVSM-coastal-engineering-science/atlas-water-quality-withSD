14dd355657eb4a405a9a91ad4f47a4b5cd5834c3
pjb-mbp15 <pjbresnahan@gmail.com>
2021-07-09 14:45:07 -0400

    Download Atlas libraries and try to add SD logging to code

--- AtlasCode_whiteboxBase.ino
+++ AtlasCode_whiteboxBase.ino
@@ -1,15 +1,30 @@
+#include <sequencer2.h>
+#include <Ezo_i2c_util.h>
+#include <sequencer1.h>
+#include <Ezo_i2c.h>
+#include <sequencer4.h>
+#include <sequencer3.h>
+#include <iot_cmd.h>
+
 #include <Wire.h>
 #include <EEPROM.h>
 
+// EZO/Atlas stuff (not sure iot_cmd is used; TODO: remove and test)
 #include <Ezo_i2c.h>
 #include <Ezo_i2c_util.h>
 #include <iot_cmd.h>
 #include <sequencer4.h>                  //imports a 4 function sequencer 
 
+// Set EZO addresses
 Ezo_board ph = Ezo_board(99, "PH");      //create a PH circuit object, who's address is 99 and name is "PH"
 Ezo_board rtd = Ezo_board(102, "TEMP"); //create an RTD Temperature circuit object who's address is 102 and name is "TEMP"
 Ezo_board DO = Ezo_board(97, "DO"); //create an DO dissolved Oxygen circuit object who's address is 97 and name is "DO"
 Ezo_board ec = Ezo_board(100, "EC");    //create an EC Salinity circuit object who's address is 100 and name is "EC"
+
+//SD stuff
+#include <SPI.h>
+#include <SD.h>
+const int chipSelect = 10;
 
 //forward declarations of functions to use them in the sequencer before defining them
 void step1 (); //read RTD cicuit
@@ -22,6 +37,14 @@ Sequencer4 Seq(&step1, 1000, &step2, 1000, &step3, 1000, &step4, 1000); //calls 
 void setup() {
   Wire.begin();                           //start the I2C
   Serial.begin(9600);                   //start the serial communication to the computer
+
+  // Initialize SD card
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    while (1);
+  }
+  
 }
 
 void loop() {
@@ -48,55 +71,54 @@ void loop() {
   ec.send_read_cmd();
   DO.send_read_cmd();
  }
- void step4() { //print data so serial monitor
-    //get the reading from the RTD circuit
-    receive_and_print_reading(rtd);  
-      if (rtd.get_error() == Ezo_board::SUCCESS);   //if the RTD reading was successful (back in step 1)
-      Serial.print("  ");
-    Serial.println();  
-   //get the reading from the PH circuit
-    receive_and_print_reading(ph);  
-      if (ph.get_error() == Ezo_board::SUCCESS);   //if the PH reading was successful (back in step 2)
-      Serial.print("  ");
-   Serial.println();  
-   //get the reading from the EC circuit
-    receive_and_print_reading(ec);            
-       if (ec.get_error() == Ezo_board::SUCCESS); //if the EC reading was successful (back in step 2)
-      Serial.print("  ");      
-   Serial.println();  
-   //get the reading from the DO circuit
-receive_and_print_reading(DO);            
-       if (DO.get_error() == Ezo_board::SUCCESS); //if the DO reading was successful (back in step 2)
-     Serial.print("  ");
- Serial.println();  
- }
- //void start_datalogging(){
-  //polling = true;                                                 //set poll to true to start the polling loop
+ void step4() { //print data to serial monitor and to SD card
 
+  // make a string for assembling the data to log:
+  String dataString = "";
+  
+  //get the reading from the RTD circuit
+  receive_and_print_reading(rtd);
+  if (rtd.get_error() == Ezo_board::SUCCESS);   //if the RTD reading was successful (back in step 1)
+    Serial.print("  ");
+  Serial.println();
+  
+  //get the reading from the PH circuit
+  receive_and_print_reading(ph);
+  if (ph.get_error() == Ezo_board::SUCCESS); //if the pH reading was successful (back in step 2)
+    Serial.print("  ");
+  Serial.println();
+  
+  //get the reading from the EC circuit
+  receive_and_print_reading(ec);
+  if (ec.get_error() == Ezo_board::SUCCESS); //if the EC reading was successful (back in step 2)
+    Serial.print("  ");
+  Serial.println();
+  
+  //get the reading from the DO circuit
+  receive_and_print_reading(DO);   
+  dataString += String(DO.get_last_received_reading(), 2);
+  if (DO.get_error() == Ezo_board::SUCCESS); //if the DO reading was successful (back in step 2)
+    Serial.print("  ");
+  Serial.println();  
 
-// function to decode the reading after the read command was issued
-void receive_reading(Ezo_board &Sensor) {
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  File dataFile = SD.open("datalog.txt", FILE_WRITE);
 
-  Serial.print(Sensor.get_name()); Serial.print(": ");  // print the name of the circuit getting the reading
-  Sensor.receive_read_cmd();                            // get the response data
-
-  switch (Sensor.get_error()) {                         // switch case based on what the response code is.
-    case Ezo_board::SUCCESS:
-      Serial.print(Sensor.get_last_received_reading()); //the command was successful, print the reading
-      break;
-
-    case Ezo_board::FAIL:
-      Serial.print("Failed ");                          //means the command has failed.
-      break;
-
-    case Ezo_board::NOT_READY:
-      Serial.print("Pending ");                         //the command has not yet been finished calculating.
-      break;
-
-    case Ezo_board::NO_DATA:
-      Serial.print("No Data ");                         //the sensor has no data to send.
-      break;
+  // if the file is available, write to it:
+  if (dataFile) {
+    dataFile.println(dataString);
+    dataFile.close();
+    
+    // print to the serial port, too:
+    Serial.println(dataString);
   }
+  
+  // if the file isn't open, pop up an error:
+  else {
+    Serial.println("error opening datalog.txt");
+  }
+  
 }
 
 // based on an example by Atlas Scientific
